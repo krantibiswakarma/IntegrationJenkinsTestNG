4b119072e7daf7390197bfdbb3c5380af75b2540
unknown <kranti.b21@gmail.com>
2018-04-19 01:53:08 -0400

    First commit

---
+++ src/test/TestWithJenkins.java
@@ -0,0 +1,11 @@
+package ExtremeTesting.JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+public class TestWithJenkins {
+  @Test
+  public void JustPrint() {
+	  
+	  System.out.println("This is a maven project");
+  }
+}
